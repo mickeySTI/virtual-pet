d0df2371c1bc1ba9a0416d0ccc5e0521268855c4
mickeySTI <imickey216@gmail.com>
2020-01-12 19:51:42 -0500

    Virtual Pet is complete, need to update README and need to work on stretcher task

--- src/main/java/mainApp/VirtualPet.java
+++ src/main/java/mainApp/VirtualPet.java
@@ -5,8 +5,9 @@ public class VirtualPet {
 	private int tiredness;
 	private int happiness;
 	private int hungriness;
-	public String petName;
 
+	public String status;
+	public String petName;
 
 	public VirtualPet(String newPetName) {
 		this.petName = newPetName;
@@ -15,72 +16,79 @@ public class VirtualPet {
 		setHungriness();
 
 	}
-		
-	
+
 	public int getTiredness() {
 		return tiredness;
 	}
-	
-	
+
 	public int getHappiness() {
 		return happiness;
 	}
 
-
 	public int getHungriness() {
 		return hungriness;
 	}
-	
-	
 
 	public void setTiredness() {
-		this.tiredness = (int) (Math.random() * 51);
+		this.tiredness = (int) (Math.random() * 51) + 0;
 
 	}
 
 	public void setHappiness() {
-		this.happiness = (int) (Math.random() * 51);
+		this.happiness = (int) (Math.random() * 51) + 0;
 
 	}
 
 	public void setHungriness() {
-		this.hungriness = (int) (Math.random() * 51);
+		this.hungriness = (int) (Math.random() * 51) + 0;
 
 	}
-	
-	
+
 	public void walk() {
-		if(tiredness < 30) {
+		if (tiredness < 30) {
 			System.out.println("You took " + petName + " out for a walk");
-		}
-		else if(happiness < 25){
+		} else if (happiness < 25) {
 			System.out.println(petName + " is too sad right now to go on a walk.");
 		}
-		
-		else if(hungriness > 25){
+
+		else if (hungriness > 25) {
 			System.out.println(petName + " is too hungry to go out for a walk. ");
 		}
-		
+
 	}
 
 	public void pet() {
 		System.out.println("You pet " + petName + " and they seem more happy now!");
-		happiness+=25;
+		happiness += 35;
+		tiredness += 5;
 	}
-	
+
 	public void feed() {
-		System.out.println("You fed " + petName + " and now they have more energy!");
+		System.out.println("You fed " + petName + " and now they are not hungry anymore and they have more energy!");
 		hungriness -= 25;
 		tiredness -= 25;
 	}
-	
-	public void tick() {
-		happiness-=15;
-		hungriness+=10;
-		tiredness+=20;
-		
+
+	public void status() {
+		System.out.println("Tiredness = " + tiredness);
+		System.out.println("Happiness = " + happiness);
+		System.out.println("Hungriness = " + hungriness);
 	}
-	
-	
+
+	public void tick() {
+		happiness -= 10;
+		if (happiness < 0) {
+			happiness = 0;
+		}
+		hungriness += 10;
+		if (hungriness < 0) {
+			hungriness = 0;
+		}
+		tiredness += 15;
+		if (tiredness < 0) {
+			tiredness = 0;
+		}
+
+	}
 
 }
--- src/main/java/mainApp/VirtualPetApp.java
+++ src/main/java/mainApp/VirtualPetApp.java
@@ -4,33 +4,60 @@ import java.util.Scanner;
 
 public class VirtualPetApp {
 
-
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to my Virtual Pet!");
 		Scanner input = new Scanner(System.in);
 		System.out.println("What would you like to name your pet?");
-		String petName = input.next();		
-		
-		
+		String petName = input.next();
+		System.out.println(petName + "? That is a great name!");
+
 		VirtualPet userPet = new VirtualPet(petName);
-		
+
 		int userChoice = 0;
+
+		System.out.println("Please choose between 1-5 ");
+		System.out.println("1 to walk " + petName + ".");
+		System.out.println("2 to feed " + petName + ".");
+		System.out.println("3 to feed " + petName + ".");
+		System.out.println("4 to get " + petName+"'s status");	
+		System.out.println("5 to exit Virtual Pet.");
 		
+
 		// game loop
-		
-		while(userChoice != 4) {
+
+		while (userChoice != 5) {
 			System.out.println("What would you like to do with " + petName + "?");
-			System.out.println("Please choose between 1-4");
-			System.out.println("1 to walk "+ petName);
-			System.out.println();
-			
-			
-			
+
+			userChoice = input.nextInt();
+
+			if (userChoice == 1) {
+				userPet.walk();
+				userPet.tick();
+			} else if (userChoice == 2) {
+				userPet.pet();
+				userPet.tick();
+			} else if (userChoice == 3) {
+				userPet.feed();
+				userPet.tick();
+			} else if (userChoice == 4) {
+				userPet.status();
+			} else if (userChoice == 5) {
+				System.out.println("Thank you for playing Virtual Pet!");
+				break;
+
+			} else {
+				System.out.println("Sorry, that's not a recognizable choice");
+				System.out.println("Please choose between 1-4");
+				userChoice = input.nextInt();
+			}
+
 		}
 		
 		
-
+		
+		
+		input.close();
 	}
 
 }
